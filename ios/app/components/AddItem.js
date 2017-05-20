ead6a79c2350865aa973d24475a5753629a8ed3c
Yzhang1994 <yzhang1994@gmail.com>
2017-05-20 11:22:10 -0700

    new styling, add loading page, add image to database and server

--- ios/app/components/AddItem.js
+++ ios/app/components/AddItem.js
@@ -30,7 +30,7 @@ class AddItem extends Component {
       notes: '',
       errorName: '',
       errorText: '',
-      image: null
+      image: "https://lh3.googleusercontent.com/YGqr3CRLm45jMF8eM8eQxc1VSERDTyzkv1CIng0qjcenJZxqV5DBgH5xlRTawnqNPcOp=w300"
     }
     this.onPressSubmit = this.onPressSubmit.bind(this);
     // this.onPressScan = this.onPressScan.bind(this);
