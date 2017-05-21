b662b5f2523e6ef58158dfad6ac2e061e25e1c6a
Anthony Cassara <acassara@gmail.com>
2017-05-20 17:05:52 -0700

    Updated server routes and hid API keys, changed hardcoded servers in front end

---
+++ config/config.dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  NUTRITIONIX_API: {
+    ID: '3d831dc9',
+    KEY: 'f3f39a4633abc4da66cf018f2c84c8f6'
+  },
+  GOOGLE_API_KEY: 'AIzaSyBJsGGirakhxDSWTCP8Tv6M2ohPL5TSPtE',
+  BING_API_KEY: '2030f380ae1448e08399595778111ed2',
+  WEB_SERVER_URL: 'http://localhost:8080'
+};
